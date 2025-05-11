8a990060535cf9eaa16e0733df80eb09591c612a
Vinod Raina <Vinod.rainajk@gmail.com>
2025-05-11 12:07:46 +0530

    Max depth

---
+++ src/trees/maxDepthBinaryTre.java
@@ -0,0 +1,16 @@
+package src.trees;
+
+public class maxDepthBinaryTre {
+
+    public int maxDepth(TreeNode root) {
+
+        if(root==null){
+            return 0;
+        }
+
+        int leftHeight = maxDepth(root.left)+1;
+        int rightHeight = maxDepth(root.right)+1;
+
+        return Math.max( leftHeight,rightHeight );
+    }
+}
