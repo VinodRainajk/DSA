136e83f94e528c07ffe4c6d3fa4daa6eac337b62
Vinod Raina <Vinod.rainajk@gmail.com>
2025-05-18 21:24:19 +0530

    Sliding window https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/

---
+++ src/slidingWindow/MaxPointFromCards.java
@@ -0,0 +1,35 @@
+package src.slidingWindow;
+
+public class MaxPointFromCards {
+    public int maxScore(int[] cardPoints, int k) {
+        int leftSum =0 ;
+
+        int maxSum = 0;
+
+        for(int idx = 0; idx < k; idx++){
+
+            leftSum = leftSum + cardPoints[idx];
+
+        }
+        maxSum = leftSum;
+
+        if(k == cardPoints.length){
+            return maxSum;
+        }
+        int jdx = k;
+        int bdx = cardPoints.length-1;
+        int rightSum = leftSum;
+        while(jdx-1 >=0){
+
+            jdx--;
+            rightSum = (rightSum - cardPoints[jdx]) + cardPoints[bdx];
+            maxSum = Math.max(rightSum,maxSum);
+            bdx--;
+        }
+
+        return maxSum;
+    }
+
+    return maxSum;
+    }
+}
