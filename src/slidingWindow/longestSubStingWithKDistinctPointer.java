a96230c73693574122f73a4521d4ca6b0e2ff65d
Vinod Raina <Vinod.rainajk@gmail.com>
2025-05-26 06:51:20 +0530

    Sliding window

--- src/slidingWindow/longestSubStingWithKDistinctPointer.java
+++ src/slidingWindow/longestSubStingWithKDistinctPointer.java
@@ -1,4 +1,33 @@
 package src.slidingWindow;
 
+import java.util.HashMap;
+
 public class longestSubStingWithKDistinctPointer {
+
+    public   int numberOfSubstrings(String s) {
+        HashMap<Character,Integer> container=  new HashMap<>();
+        int count =0;
+
+        container.put('a',-1);
+        container.put('b',-1);
+        container.put('c',-1);
+
+        for (int idx =0; idx < s.length() ; idx++){
+            if(s.charAt(idx)=='a'){
+                container.put('a',idx);
+            }
+
+            if(s.charAt(idx)=='b'){
+                container.put('b',idx);
+            }
+
+            if(s.charAt(idx)=='c'){
+                container.put('c',idx);
+            }
+
+            count = count + (Math.min(Math.min(container.get('a'), container.get('b')),container.get('c'))+1);
+        }
+
+        return count;
+    }
 }
